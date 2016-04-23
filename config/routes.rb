83bc926abbd7068a926ca87cc59601807e33b3d0
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-23 11:17:51 +0300

    Generate administrate panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :answers
+resources :questions
+resources :subjects
+resources :tests
+
+    root to: "answers#index"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
