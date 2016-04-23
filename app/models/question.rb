bab337299db63607d0b252bc2f3a162143f3868e
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-23 11:17:22 +0300

    Annotate models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  question   :string
+#  test_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Question < ActiveRecord::Base
   belongs_to :test
   has_many :answers
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tests
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  subject_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Test < ActiveRecord::Base
   belongs_to :subject
   has_many :questions
